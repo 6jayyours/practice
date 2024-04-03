e6720a898c0228693028deec2f97e7b0f8e307e5
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-04-03 09:46:59 +0530

    first_problem_test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s = "PAYPALISHIRING";
+        int numRows = 4;
+
+        StringBuilder out = new StringBuilder();
+
+        int k = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            int j = i;
+            while (j < s.length()) {
+                out.append(s.charAt(j));
+                if (i != 0 && i != numRows - 1) {
+                    int temp = j + k - 2 * i;
+                    if (temp < s.length())
+                        out.append(s.charAt(temp));
+                }
+                j += k;
+            }
+        }
+        String a = out.toString();
     }
 }
