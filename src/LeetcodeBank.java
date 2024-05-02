410c72e8e08820f98394046425e287718ad0f05d
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-05-02 11:37:01 +0530

    leetcode1716

--- src/LeetcodeBank.java
+++ src/LeetcodeBank.java
@@ -1,2 +1,32 @@
 public class LeetcodeBank {
+    public static void main(String[] args) {
+        System.out.println("leetcode : 1716");
+        int n = 20;
+        int result = totalMoney(n);
+        System.out.println(result);
+    }
+
+    private static int totalMoney(int n) {
+        int money =0;
+        int start = 1;
+        int i = 1;
+        int day = 1;
+
+        while(day<=n) {
+            if(day % 7  == 0) {
+                money = money + i;
+                day++;
+                start++;
+                i = start;
+            }
+            if(day<=n) {
+                money = money + i;
+                i++;
+                day++;
+            }
+
+        }
+
+        return money;
+    }
 }
